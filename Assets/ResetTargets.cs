bdac89466350be312e87ad8fb5a87a85cad40384
Dylan <dylanryan431@gmail.com>
2023-10-23 10:42:19 -0300

    Changes to start room

--- Assets/ResetTargets.cs
+++ Assets/ResetTargets.cs
@@ -2,26 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
 
 public class ResetTargets : MonoBehaviour
 {
-    public GameObject red;
-    public GameObject blue;
-    public GameObject player;
-
-    private void PlayerTransform()
-    {
-        player.transform.position = new Vector3(23f, 0, 21);
-        player.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
-    }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("SampleScene");
-
+            SceneManager.LoadSceneAsync("SampleScene");
         }
-        PlayerTransform();
     }
 }
 
