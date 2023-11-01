caca0ba41ba2607ccd4910d4bfc968fc73d5a20a
Dylan <dylanryan431@gmail.com>
2023-11-01 09:43:49 -0300

    Added functioning timer

--- Assets/Scripts/TimeScore.cs
+++ Assets/Scripts/TimeScore.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements.Experimental;
 using TMPro;
+using Unity.VisualScripting;
 
 public class TimeScore : MonoBehaviour
 {
-    public float timerScore;
+    public float timerScore = 0.0f;
+    public int timerSeconds;
     public TextMeshProUGUI timerText;
+    public int endTimeScore;
+    public TextMeshProUGUI fakeTimerText;
     // Start is called before the first frame update
     void Start()
+    {
+        fakeTimerText.gameObject.SetActive(true);
+        timerText.gameObject.SetActive(false);
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void OnTriggerEnter(Collider other)
+    void Timer()
     {
-        if (other.gameObject.tag == ("Player"))
-            timerScore += Time.deltaTime;
-            timerText.text = (timerScore).ToString("0");
+        timerScore += Time.deltaTime;
+        timerSeconds = (int)(timerScore % 60);
+        timerText.text = (timerSeconds).ToString("0");
+    }
+    // Update is called once per frame
+    void OnTriggerStay(Collider other)
+    {
+        
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            timerSeconds -= timerSeconds;
+            Timer();
+
+            fakeTimerText.gameObject.SetActive(false);
+            timerText.gameObject.SetActive (true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            endTimeScore = timerSeconds;
+            timerText.text = (endTimeScore).ToString("0");
+        }
     }
 }
